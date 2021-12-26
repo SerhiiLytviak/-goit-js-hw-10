49d0b64ab4bfac42b5651f53f6d6b71cc311a629
SerhiiLytviak <litvyak.sergey@gmail.com>
2021-12-26 18:25:58 +0200

    Подключил шаблоны
    Подключены шаблоны

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ var debounce = require('lodash.debounce');
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 import API from './fetchCountries';
+import countryListTpl from './templates/country-list';
+import countryCardTpl from './templates/country-card';
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
@@ -22,11 +24,11 @@ function onSearch(e) {
   }
 
   API.fetchCountries(searchQuery)
-    .then(renderItems)
+    .then(renderSearchResults)
     .catch(error => console.log(error));
 }
 
-function renderItems(countries) {
+function renderSearchResults(countries) {
   refs.countryInfo.innerHTML = '';
   refs.countryList.innerHTML = '';
 
@@ -39,65 +41,47 @@ function renderItems(countries) {
   }
 }
 
-function renderCountryInfo(country) {
-  return refs.countryInfo.insertAdjacentHTML(
-    'afterbegin',
-    `
-            <div class="country-header">
-                <img src="${country[0].flags.svg}" alt="flag" width="50" heigth="30">
-                <h1 class="country-name">${country[0].name.official}</h1>
-            </div>
-                <p><b>Capital:</b> ${country[0].capital}</p>
-                <p><b>Population:</b> ${country[0].population}</p>
-                <p><b>Languages:</b> ${Object.values(country[0].languages)}</p>
-    `,
-  );
-}
-
 function renderCountryList(countries) {
   countries.map(country => {
-    refs.countryList.insertAdjacentHTML(
-      'afterbegin',
-      `<li>
-               <div class="country-header">
-                   <img src="${country.flags.svg}" alt="flag" width="40" height="25">
-                   <h3 class="country-name">${country.name.official}</h3>
-               </div>
-           </li>`,
-    );
+    refs.countryList.insertAdjacentHTML('afterbegin', countryListTpl(country));
   });
 }
+
+function renderCountryInfo(country) {
+  country.map(country => {
+    return refs.countryInfo.insertAdjacentHTML('afterbegin', countryCardTpl(country));
+  });
+}
+// function renderCountryInfo(country) {
+//   return refs.countryInfo.insertAdjacentHTML(
+//     'afterbegin',
+//     `
+//             <div class="country-header">
+//                 <img src="${country[0].flags.svg}" alt="flag" width="50" heigth="30">
+//                 <h1 class="country-name">${country[0].name.official}</h1>
+//             </div>
+//                 <p><b>Capital:</b> ${country[0].capital}</p>
+//                 <p><b>Population:</b> ${country[0].population}</p>
+//                 <p><b>Languages:</b> ${Object.values(country[0].languages)}</p>
+//     `,
+//   );
+// }
+
+// function renderCountryList(countries) {
+//   countries.map(country => {
+//     refs.countryList.insertAdjacentHTML(
+//       'afterbegin',
+//       `<li>
+//                <div class="country-header">
+//                    <img src="${country.flags.svg}" alt="flag" width="40" height="25">
+//                    <h3 class="country-name">${country.name.official}</h3>
+//                </div>
+//            </li>`,
+//     );
+//   });
+// }
 
 //   }
 //   const markUp = countryCardTpl(countries);
 //   refs.countryInfo.innerHTML = markUp;
-// }
-
-// function countryCardTpl(countries) {
-//   if (countries.length === 1) {
-//     return `
-//             <div class="country-header">
-//                 <img src="${countries[0].flags.svg}" alt="flag" width="30" heigth="50">
-//                 <h2>${countries[0].name.official}</h2>
-//             </div>
-//                 <p>Capital: ${countries[0].capital}</p>
-//                 <p>Population: ${countries[0].population}</p>
-//                 <p>Languages: ${Object.values(countries[0].languages)}</p>
-//     `;
-//   } else if (countries.length > 10) {
-//     Notify.info('Too many matches found. Please enter a more specific name.');
-//   } else if (1 < countries.length <= 10) {
-//     console.log(countries.length);
-//     countries.map(country => {
-//       refs.countryList.insertAdjacentHTML(
-//         'afterend',
-//         `<li>
-//                 <div class="country-header">
-//                     <img src="${country.flags.svg}" alt="flag" width="30" heigth="50">
-//                     <h2>${country.name.official}</h2>
-//                 </div>
-//             </li>`,
-//       );
-//     });
-//   }
-// }
+//
